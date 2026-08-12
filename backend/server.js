2071380c1603b85710bc7eb9a2d5345e3c3b6a34
salvador260 <artingozo2@gmail.com>
2026-08-12 20:41:04 +0430

    fix render port

--- backend/server.js
+++ backend/server.js
@@ -181,6 +181,8 @@ app.delete("/orders/:id", async (req, res) => {
 
 
 // اجرای سرور
-app.listen(3000, () => {
-    console.log("Backend running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Backend running on port ${PORT}`);
 });
